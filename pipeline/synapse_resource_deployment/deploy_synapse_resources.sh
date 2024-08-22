de3976b4605ab96fd326f085863ac7c082895cf5
Willem Jongeneel <willemjongeneel@MBP-van-Willem.home>
2024-08-22 15:32:08 +0200

    update

--- pipeline/synapse_resource_deployment/deploy_synapse_resources.sh
+++ pipeline/synapse_resource_deployment/deploy_synapse_resources.sh
@@ -6,10 +6,9 @@ az synapse notebook create \
     --name load_customer_statement_records \
     --file @"../python/load_customer_statement_records.ipynb" 
 
-# deploy pipeline, ${RANDOM} is used, because if you break down the infrastructure
-# and redeploy with the same name, the pipeline will fail 
+# deploy pipeline
 az synapse pipeline create \
     --workspace-name codingassignmentsynapse \
-    --name "load_customer_statement_records_${RANDOM}" \
+    --name "load_customer_statement_records" \
     --file @"synapse_files/pipeline.json" 
 
